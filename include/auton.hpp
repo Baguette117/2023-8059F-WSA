fe64e8dbd3784469569e8f45f66c94becc12db9b
student USER <ian@popoheads.com>
2023-07-17 21:47:52 +0800

    Auton framework finished, untested

--- include/auton.hpp
+++ include/auton.hpp
@@ -1,3 +1,9 @@
+typedef enum pathEnum {
+	pathEnum_MOVE = 0,
+    pathEnum_TURN = 1,
+    pathEnum_All = 2,
+} pathEnumT;
+
 void autonPID(void *ignore);
-void calibration();
+void calibration(pathEnumT path);
 void path1();
--- src/auton.cpp
+++ src/auton.cpp
@@ -1,6 +1,9 @@
 #include "main.h"
+#include "pros/rtos.h"
 #define akp .4
 #define akd 0
+#define akt 2.5 //Degrees of wheel turn to turn base 1 degree
+#define akm 28.6478897565 //Degrees of wheel turn to 1 inch
 
 Motor left1 (left1Port, MOTOR_GEAR_GREEN, false, MOTOR_ENCODER_DEGREES);
 Motor left2 (left2Port, MOTOR_GEAR_GREEN, true, MOTOR_ENCODER_DEGREES);
@@ -8,7 +11,7 @@ Motor right1 (right1Port, MOTOR_GEAR_GREEN, true, MOTOR_ENCODER_DEGREES);
 Motor right2 (right2Port, MOTOR_GEAR_GREEN, false, MOTOR_ENCODER_DEGREES);
 Motor intake (intakePort, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_DEGREES);
 
-bool voltControl = false;
+bool voltControl = false, targReach = false;
 double targLeft = 0, targRight = 0, errLeft, errRight, prevErrLeft = 0, prevErrRight = 0, derivLeft, derivRight;
 
 void autonPID(void *ignore){
@@ -34,6 +37,59 @@ void autonPID(void *ignore){
             delay(5);
         }
 
+        targReach = -15 < errLeft && errLeft < 15 && -15 < errRight && errRight < 15;
+
         delay(15);
+    }
+}
+
+void move(double inches, int time = 0){
+    targLeft += inches;
+    targRight += inches;
+    printf("Move: %f\n", inches);
+
+    delay(500);
+
+    if (time){
+        delay(time);
+    } else {
+        while (!targReach) {
+            printf("Moving\n");
+            delay(20);
+        }
+    }
+}
+
+void turn(double degrees, int time = 0){
+    targLeft += degrees;
+    targRight -= degrees;
+    printf("Turning: %f\n", degrees);
+
+    delay(500);
+
+    if (time){
+        delay(time);
+    } else {
+        while (!targReach) {
+            printf("Turning\n");
+            delay(20);
+        }
+    }
+}
+
+void calibration(pathEnumT path){
+    Task autonPIDTask (autonPID, (void*)"BALLS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "autonPIDTask");
+    
+    switch(path){
+        case pathEnum_MOVE:
+            move(24);
+            break;
+        case pathEnum_TURN:
+            turn(90);
+            break;
+        case pathEnum_All:
+            move(24);
+            turn(90);
+            break;
     }
 }
